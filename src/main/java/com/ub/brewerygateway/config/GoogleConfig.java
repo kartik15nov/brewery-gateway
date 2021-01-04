5c606dcf97b5305ce0051631a173c2a628736225
Kartik <kartik15nov@gmail.com>
2021-01-04 23:39:36 +0530

    Fixed the java based configuration. Added property based configuration for spring cloud gateway.

--- src/main/java/com/ub/brewerygateway/config/GoogleConfig.java
+++ src/main/java/com/ub/brewerygateway/config/GoogleConfig.java
@@ -2,15 +2,18 @@ package com.ub.brewerygateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("google")
 @Configuration
 public class GoogleConfig {
+
+    @Bean
     public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("google", r -> r.path("/google5")
+                .route("googleD", r -> r.path("/google5")
                         .filters(f -> f.rewritePath("/google5(?<segment>/?.*)", "/${segment}"))
                         .uri("https://www.google.com/"))
                 .build();
