77c2a9ed857dff872813297f0cd055adf60626aa
Kartik <kartik15nov@gmail.com>
2021-02-14 09:36:54 +0530

    Configured the gateway to use the Hystrix circuit breaker, and downgraded the spring version for that.

--- src/main/java/com/ub/brewerygateway/config/LocalDiscovery.java
+++ src/main/java/com/ub/brewerygateway/config/LocalDiscovery.java
@@ -1,11 +1,13 @@
 package com.ub.brewerygateway.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("local-discovery")
 @EnableDiscoveryClient
+@EnableHystrix
 @Configuration
 public class LocalDiscovery {
 }
